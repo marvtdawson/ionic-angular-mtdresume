7979969e8a83333d04ec387f7fd9f6771bc6cf34
Marvin Dawson <marvintdawson@gmail.com>
2021-07-06 12:00:10 -0500

    refactored routes added them to menu routing as children

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  // {
+  //   path: '',
+  //   loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  // },
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
   },
   {
     path: 'home',
--- src/app/pages/menu/menu-routing.module.ts
+++ src/app/pages/menu/menu-routing.module.ts
@@ -1,57 +1,83 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 
-import { MenuPage } from './menu.page';
+import {MenuPage} from './menu.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: MenuPage
-  },
-  {
-    path: 'home',
-    loadChildren: () => import('../home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: 'tabs',
-    loadChildren: () => import('../../tabs/tabs.module').then(m => m.TabsPageModule)
-  },
-
-  {
-    path: 'login',
-    loadChildren: () => import('../../pages/login/login.module').then( m => m.LoginPageModule)
-  },
-  {
-    path: 'logout',
-    loadChildren: () => import('../../pages/logout/logout.module').then( m => m.LogoutPageModule)
-  },
-  {
-    path: 'contact',
-    loadChildren: () => import('../../pages/contact/contact.module').then( m => m.ContactPageModule)
-  },
-  {
-    path: 'subscribe',
-    loadChildren: () => import('../../pages/subscribe/subscribe.module').then( m => m.SubscribePageModule)
-  },
-  {
-    path: 'blog',
-    loadChildren: () => import('../../pages/blog/blog.module').then( m => m.BlogPageModule)
-  },
-  {
-    path: 'mobile-app',
-    loadChildren: () => import('../../pages/mobile-app/mobile-app.module').then( m => m.MobileAppPageModule)
-  },
-  {
-    path: 'terms-conditions',
-    loadChildren: () => import('../../pages/terms-conditions/terms-conditions.module').then( m => m.TermsConditionsPageModule)
-  },
-  {
-    path: 'private-policy',
-    loadChildren: () => import('../../pages/private-policy/private-policy.module').then( m => m.PrivatePolicyPageModule)
-  },
-  {
-    path: 'page-not-found',
-    loadChildren: () => import('../../pages/page-not-found/page-not-found.module').then( m => m.PageNotFoundPageModule)
+    component: MenuPage,
+    children: [
+      {
+        path: '',
+        loadChildren: () => import('../home/home.module').then(m => m.HomePageModule)
+      },{
+        path: 'home',
+        loadChildren: () => import('../home/home.module').then(m => m.HomePageModule)
+      },
+      {
+        path: 'tabs',
+        loadChildren: () => import('../../tabs/tabs.module').then(m => m.TabsPageModule),
+        children: [
+          {
+            path: 'tab1',
+            loadChildren: () => import('../../tab1/tab1.module').then(m => m.Tab1PageModule)
+          },
+          {
+            path: 'tab2',
+            loadChildren: () => import('../../tab2/tab2.module').then(m => m.Tab2PageModule)
+          },
+          {
+            path: 'tab3',
+            loadChildren: () => import('../../tab3/tab3.module').then(m => m.Tab3PageModule)
+          },
+          // {
+          //   path: '',
+          //   redirectTo: '/tabs/tab1',
+          //   pathMatch: 'full'
+          // }
+        ]
+      },
+      {
+        path: 'contact',
+        loadChildren: () => import('../../pages/contact/contact.module').then(m => m.ContactPageModule)
+      },{
+        path: 'subscribe',
+        loadChildren: () => import('../../pages/subscribe/subscribe.module').then(m => m.SubscribePageModule)
+      },
+      {
+        path: 'blog',
+        loadChildren: () => import('../../pages/blog/blog.module').then(m => m.BlogPageModule)
+      },
+      {
+        path: 'mobile-app',
+        loadChildren: () => import('../../pages/mobile-app/mobile-app.module').then(m => m.MobileAppPageModule)
+      },
+      {
+        path: 'terms-conditions',
+        loadChildren: () => import('../../pages/terms-conditions/terms-conditions.module').then(m => m.TermsConditionsPageModule)
+      },
+      {
+        path: 'private-policy',
+        loadChildren: () => import('../../pages/private-policy/private-policy.module').then(m => m.PrivatePolicyPageModule)
+      },
+      {
+        path: 'login',
+        loadChildren: () => import('../../pages/login/login.module').then(m => m.LoginPageModule)
+      },
+      {
+        path: 'logout',
+        loadChildren: () => import('../../pages/logout/logout.module').then(m => m.LogoutPageModule)
+      },
+      {
+        path: 'page-not-found',
+        loadChildren: () => import('../../pages/page-not-found/page-not-found.module').then(m => m.PageNotFoundPageModule)
+      },
+      {
+        path: '**',
+        loadChildren: () => import('../../pages/page-not-found/page-not-found.module').then(m => m.PageNotFoundPageModule)
+      }
+    ]
   }
 ];
 
@@ -59,4 +85,5 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class MenuPageRoutingModule {}
+export class MenuPageRoutingModule {
+}
