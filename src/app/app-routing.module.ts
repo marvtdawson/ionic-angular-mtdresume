dff94ee2f7646ec0b21164bccdc312b6eae81bf3
Marvin Dawson <marvintdawson@gmail.com>
2021-07-06 10:56:59 -0500

    added component dir and shared-component module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'menu',
+    loadChildren: () => import('./pages/menu/menu.module').then( m => m.MenuPageModule)
   }
 ];
 @NgModule({
