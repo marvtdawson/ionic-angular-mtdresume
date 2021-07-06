b146bfe5f91749eec2b41824bd27fc7c6c76a643
Marvin Dawson <marvintdawson@gmail.com>
2021-07-06 11:16:32 -0500

    added home page to menu-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,32 +7,8 @@ const routes: Routes = [
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   },
   {
-    path: 'menu',
-    loadChildren: () => import('./pages/menu/menu.module').then( m => m.MenuPageModule)
-  },
-  {
     path: 'home',
     loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: 'login',
-    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
-  },
-  {
-    path: 'logout',
-    loadChildren: () => import('./pages/logout/logout.module').then( m => m.LogoutPageModule)
-  },
-  {
-    path: 'contact',
-    loadChildren: () => import('./pages/contact/contact.module').then( m => m.ContactPageModule)
-  },
-  {
-    path: 'subscribe',
-    loadChildren: () => import('./pages/subscribe/subscribe.module').then( m => m.SubscribePageModule)
-  },
-  {
-    path: 'blog',
-    loadChildren: () => import('./pages/blog/blog.module').then( m => m.BlogPageModule)
   }
 ];
 @NgModule({
--- src/app/pages/menu/menu-routing.module.ts
+++ src/app/pages/menu/menu-routing.module.ts
@@ -7,6 +7,30 @@ const routes: Routes = [
   {
     path: '',
     component: MenuPage
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('../home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('../../pages/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'logout',
+    loadChildren: () => import('../../pages/logout/logout.module').then( m => m.LogoutPageModule)
+  },
+  {
+    path: 'contact',
+    loadChildren: () => import('../../pages/contact/contact.module').then( m => m.ContactPageModule)
+  },
+  {
+    path: 'subscribe',
+    loadChildren: () => import('../../pages/subscribe/subscribe.module').then( m => m.SubscribePageModule)
+  },
+  {
+    path: 'blog',
+    loadChildren: () => import('../../pages/blog/blog.module').then( m => m.BlogPageModule)
   }
 ];
 
