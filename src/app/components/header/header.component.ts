e614393b839915c2e6bd02f2cb5d65c9f4ab77b4
Marvin Dawson <marvintdawson@gmail.com>
2021-07-10 18:25:10 -0500

    changed the desciption on package json

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
+import {Observable} from 'rxjs';
+import {SiteDataService} from '../../../services/site-data.service';
 
 @Component({
   selector: 'app-header',
@@ -6,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  @Input()pageTitle: string;
+  dropdown = true;
+  accountDropDown = false;
+  appName = this.siteData.siteName;
+  appLogo = this.siteData.siteLogo;
+  isUserLoggedOut$: Observable<boolean>;
+  isUserLoggedIn$: Observable<boolean>;
+  displayName$: any;
 
-  constructor() { }
+  @ViewChild('eventsBtn', {read: ElementRef}) eventBtn: ElementRef;
+  @ViewChild('accountBtn', {read: ElementRef}) accountBtn: ElementRef;
+
+  constructor(private siteData: SiteDataService) {
+  }
 
   ngOnInit() {}
+
+  hideDropdown(event) {
+    const xTouch = event.clientX;
+    const yTouch = event.clientY;
+
+    const rect = this.eventBtn.nativeElement.getBoundingClientRect();
+    const topBoundary = rect.top + 2;
+    const leftBoundary = rect.top + 2;
+    const rightBoundary = rect.top + 2;
+
+    if(xTouch < leftBoundary || xTouch > rightBoundary || yTouch < topBoundary) {
+      this.dropdown = false;
+    }
+  }
 
 }
