5c9180b8d01cb1fd300a53de7bf2d41831e13ca2
Marvin Dawson <marvintdawson@gmail.com>
2021-07-11 17:24:27 -0500

    added jumbrtron component and scss

---
+++ src/app/components/jumbotron/jumbotron.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+
+import { JumbotronComponent } from './jumbotron.component';
+
+describe('JumbotronComponent', () => {
+  let component: JumbotronComponent;
+  let fixture: ComponentFixture<JumbotronComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ JumbotronComponent ],
+      imports: [IonicModule.forRoot()]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(JumbotronComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
