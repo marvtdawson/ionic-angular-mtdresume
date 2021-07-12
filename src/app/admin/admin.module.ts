98cfcca0532c4fc4bb2c58a7f721a1fc0a1bcedd
Marvin Dawson <marvintdawson@gmail.com>
2021-07-12 01:32:32 -0500

    added header component to admin page

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -7,14 +7,16 @@ import { IonicModule } from '@ionic/angular';
 import { AdminPageRoutingModule } from './admin-routing.module';
 
 import { AdminPage } from './admin.page';
+import {SharedComponentsModule} from "../components/shared-components/shared-components.module";
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    AdminPageRoutingModule
-  ],
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        AdminPageRoutingModule,
+        SharedComponentsModule
+    ],
   declarations: [AdminPage]
 })
 export class AdminPageModule {}
