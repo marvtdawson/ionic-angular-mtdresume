76b68a854406a0be62a11c889ba3eb9a5938fffa
Marvin Dawson <marvintdawson@gmail.com>
2021-07-13 00:43:10 -0500

    added privacy and terms content

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -14,61 +14,73 @@ export class HomePage implements OnInit {
       thumbnailImg: true,
       linkPath: '/mattresses',
       linkImage: '../../assets/img/matts/beautyrest_black_mariela.png',
-      linkText: 'Mattress'
+      linkText: 'Allergy and Immunology'
     },
     {
       thumbnailImg: true,
       linkPath: '/bedrooms',
       linkImage: '../../assets/img/bed_rm/Tabasco_Storage_King_Bed_Styled.jpg',
-      linkText: 'Bedrooms'
+      linkText: 'Alternative Medicine'
     },
     {
       thumbnailImg: true,
       linkPath: '/living-rooms',
       linkImage: '../../assets/img/liv_rm/Compact-apartment-Moscow.jpg',
-      linkText: 'Living Rooms'
+      linkText: 'Anesthesiology'
     },
     {
       thumbnailImg: true,
       linkPath: '/kitchens',
       linkImage: './assets/img/kitch/kitchen_image_2.jpg',
-      linkText: 'Kitchens'
+      linkText: 'Colon and Rectal Surgery'
     },
     {
       thumbnailImg: true,
       linkPath: '/dining-rooms',
       linkImage: './assets/img/dining_rm/dining-tables-slide-0XQB-jumbo.jpg',
-      linkText: 'Dining Rooms'
+      linkText: 'Dental'
     },
     {
       thumbnailImg: true,
       linkPath: '/bathrooms',
       linkImage: './assets/img/bath_rm/bathroom_1.jpg',
-      linkText: 'Bathrooms'
+      linkText: 'Dermatology'
     },
     {
       thumbnailImg: true,
       linkPath: '/lamps',
       linkImage: './assets/img/lamps/1-splash-mobile-Table-Lamps.jpg',
-      linkText: 'Lamps'
+      linkText: 'Dietcian'
     },
     {
       thumbnailImg: true,
       linkPath: '/tables',
       linkImage: './assets/img/tables/tyler-creek-counter-height-dining-room-table-0.jpg',
-      linkText: 'Tables'
+      linkText: 'Emergency Medicine'
     },
     {
       thumbnailImg: true,
       linkPath: '/recliners',
       linkImage: './assets/img/recliners/lansing-tufted-leather-recliner-with-nailheads-o.jpg',
-      linkText: 'Recliners'
+      linkText: 'Family Medicine'
     },
     {
       thumbnailImg: true,
       linkPath: '/accessories',
       linkImage: './assets/img/accessories/eb079052b1972b81297106d1c55c573e.jpg',
-      linkText: 'Accessories'
+      linkText: 'Internal Medicine'
+    },
+    {
+      thumbnailImg: true,
+      linkPath: '/accessories',
+      linkImage: './assets/img/accessories/eb079052b1972b81297106d1c55c573e.jpg',
+      linkText: 'Medical Facilitation'
+    },
+    {
+      thumbnailImg: true,
+      linkPath: '/accessories',
+      linkImage: './assets/img/accessories/eb079052b1972b81297106d1c55c573e.jpg',
+      linkText: 'Genetics and Genomics'
     }
   ];
   constructor(private router: Router) { }
