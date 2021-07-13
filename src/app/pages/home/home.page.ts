7ef57105631662abe3685bed4979778a0c4b3d0c
Marvin Dawson <marvintdawson@gmail.com>
2021-07-12 22:46:36 -0500

    removed call to header in pages

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -9,6 +9,68 @@ import {Router} from '@angular/router';
 export class HomePage implements OnInit {
 
   // customerSatisfactionImage = '';
+  public departments = [
+    {
+      thumbnailImg: true,
+      linkPath: '/mattresses',
+      linkImage: '../../assets/img/matts/beautyrest_black_mariela.png',
+      linkText: 'Mattress'
+    },
+    {
+      thumbnailImg: true,
+      linkPath: '/bedrooms',
+      linkImage: '../../assets/img/bed_rm/Tabasco_Storage_King_Bed_Styled.jpg',
+      linkText: 'Bedrooms'
+    },
+    {
+      thumbnailImg: true,
+      linkPath: '/living-rooms',
+      linkImage: '../../assets/img/liv_rm/Compact-apartment-Moscow.jpg',
+      linkText: 'Living Rooms'
+    },
+    {
+      thumbnailImg: true,
+      linkPath: '/kitchens',
+      linkImage: './assets/img/kitch/kitchen_image_2.jpg',
+      linkText: 'Kitchens'
+    },
+    {
+      thumbnailImg: true,
+      linkPath: '/dining-rooms',
+      linkImage: './assets/img/dining_rm/dining-tables-slide-0XQB-jumbo.jpg',
+      linkText: 'Dining Rooms'
+    },
+    {
+      thumbnailImg: true,
+      linkPath: '/bathrooms',
+      linkImage: './assets/img/bath_rm/bathroom_1.jpg',
+      linkText: 'Bathrooms'
+    },
+    {
+      thumbnailImg: true,
+      linkPath: '/lamps',
+      linkImage: './assets/img/lamps/1-splash-mobile-Table-Lamps.jpg',
+      linkText: 'Lamps'
+    },
+    {
+      thumbnailImg: true,
+      linkPath: '/tables',
+      linkImage: './assets/img/tables/tyler-creek-counter-height-dining-room-table-0.jpg',
+      linkText: 'Tables'
+    },
+    {
+      thumbnailImg: true,
+      linkPath: '/recliners',
+      linkImage: './assets/img/recliners/lansing-tufted-leather-recliner-with-nailheads-o.jpg',
+      linkText: 'Recliners'
+    },
+    {
+      thumbnailImg: true,
+      linkPath: '/accessories',
+      linkImage: './assets/img/accessories/eb079052b1972b81297106d1c55c573e.jpg',
+      linkText: 'Accessories'
+    }
+  ];
   constructor(private router: Router) { }
 
   ngOnInit() {
